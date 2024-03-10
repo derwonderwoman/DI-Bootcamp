f4b7541b116d7c7d75a3188f73285752096abbbd
derwonderwoman <tokarevakseniia@gmail.com>
2024-03-10 11:19:49 +0200

    Redo Matrix

--- Week2/Day4/Challenge/main.py
+++ Week2/Day4/Challenge/main.py
@@ -1,8 +1,23 @@
-matrix = [['7','T','h','i','s','$','#','^'],['i','s','%',' ','M','a','t','r'],['i','x','?','#',' ',' ','%','!']]
+string = "7iiTsxh%?i #sM $a #t%^r!"
+ROWS = 3
+COLUMNS = 8
+matrix = []
+new_matrix = []
+
+for i in range(ROWS):
+    row = []
+    for j in range(COLUMNS):
+        row.append(string[i + j * ROWS])
+    new_matrix.append(row)
+
+
+print(new_matrix)
+
+# matrix = [['7','T','h','i','s','$','#','^'],['i','s','%',' ','M','a','t','r'],['i','x','?','#',' ',' ','%','!']]
 text = []
 flag = 0
 
-for i in matrix:
+for i in new_matrix:
     for s in i:
         symb = ord(s)
         if symb in range(65,91) or symb in range(97, 123):
