721f93155ebf3c87dbe049fdf7ee5aa36dec35fe
derwonderwoman <tokarevakseniia@gmail.com>
2024-05-23 19:21:01 +0300

    todo

--- Week5/Day4/challenge/challenge.js
+++ Week5/Day4/challenge/challenge.js
@@ -1,36 +1,37 @@
 const tasks = []
 let form = document.forms[0]
-let title = document.createElement('div')
-title.textContent = "TO-DO-LIST"
-title.style.color = 'white'
-title.style.fontWeight = 'strong'
-title.style.fontSize = '60px'
-title.style.textAlign = 'center'
+form.addEventListener("submit", handleSubmit);
+const image = href="./xmark-solid.svg"
 
-form.append(title)
-
-function addTask(){
-    let input = document.querySelector('#name').textContent
-    let list = document.querySelector('.listTasks')
-    if(input != 'none'){
-        tasks.push(input)
-        let taskElement = document.createElement('li')
-        taskElement.textContent = input
-        list.appendChild(input)
+function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const task = formData.get("task");
+    if(task !== ''){
+        tasks.push(task);
+        renderTasks(tasks);
     }
+    form.reset();
+}
+const ul = document.getElementById("listTasks");
+ 
+
+function renderTasks(tasks){
+    console.log("tasks:", tasks);
+    const task = tasks[tasks.length - 1];
+    const li = generateLi(task);
+    ul.append(li);
 }
 
+function generateLi(task) {
+    const li = document.createElement("li");
+    const pic = document.createElement("i");
+    pic.classList.add("fa-solid", "fa-xmark")
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    const span = document.createElement("span")
+    span.innerText = task;
 
-// then add it to the DOM, below the form (in the <div class="listTasks"></div>) .
-// Each new task added should have (starting from left to right - check out the image at the top of the page)
-// a “X” button. Use font awesome for the “X” button.
-// an input type="checkbox". The label of the input is the task added by the user.
-
-// BONUS I (not mandatory):
-// Change the variable tasks to an array of task objects.
-// Each new task added to the array should have the properties : task_id, text and done (a boolean - false by default).
-// Every new task object should have a task_id, starting from 0, and a data-task-id attribute, which value is the same as the task_id. Check out data-* attributes here.
-// Create a function named doneTask(), that as soon as the user clicks on the “checkbox” input, the done property should change from false to true in the object, and from black to crossed out red in the DOM.
-
-// BONUS II (not mandatory):
-// Create a function named deleteTask(), that as soon as the user clicks on the “X” button, delete that specific task from the array listTasks.
+    li.append(pic, checkbox, span);
+    return li;
+}
